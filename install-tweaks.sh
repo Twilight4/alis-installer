0790cc0c3e1df0ebb540af7768961e9ddd1fa0f5
Twilight4 <electrolight071@gmail.com>
2023-05-19 14:39:13 +0200

    update

--- install-tweaks.sh
+++ install-tweaks.sh
@@ -125,16 +125,16 @@ EOF
 
 chmod 600 /etc/NetworkManager/conf.d/ip6-privacy.conf
 
-# Improve virtual memory performance
-bash -c 'cat > /etc/sysctl.d/98-misc.conf' <<-'EOF'
-vm.dirty_background_ratio=15
-vm.dirty_ratio=40
-vm.oom_dump_tasks=0
-vm.oom_kill_allocating_task=1
-vm.overcommit_memory=1
-vm.swappiness=10
-vm.vfs_cache_pressure=50
-EOF
+# Improve virtual memory performance - CachyOS has probably better settings built-in
+#bash -c 'cat > /etc/sysctl.d/98-misc.conf' <<-'EOF'
+#vm.dirty_background_ratio=15
+#vm.dirty_ratio=40
+#vm.oom_dump_tasks=0
+#vm.oom_kill_allocating_task=1
+#vm.overcommit_memory=1
+#vm.swappiness=10
+#vm.vfs_cache_pressure=50
+#EOF
 
 ######################################################################
 # Configuring the system
